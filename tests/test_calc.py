e2be85ed8f8d4fb0e113a3b84e16a7bf7ad82df2
Muller Jorge <millerjmatos@hotmail.com>
2024-11-28 20:22:50 -0300

    formatting with autopep8

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -2,15 +2,17 @@
 import unittest
 from src.calculadora import Calculadora
 
+
 class TestCalculadora(unittest.TestCase):
     def setUp(self):
         self.calc = Calculadora()
-    
+
     def test_soma(self):
         self.assertEqual(self.calc.soma(2, 3), 5)
-    
+
     def test_subtracao(self):
         self.assertEqual(self.calc.subtracao(5, 3), 2)
+
 
 if __name__ == '__main__':
     unittest.main()
